31d0fffe6fdc26f007b9e40966b20414fd622d4c
RUPAM LAHA <rupamlaha@RUPAMs-MacBook-Air.local>
2022-01-22 11:21:15 +0530

    setting up proj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './UserComponents/user-login/user-login.component';
+import { UserRegistrationComponent } from './UserComponents/user-registration/user-registration.component';
+import { SellerLoginComponent } from './SellerComponents/seller-login/seller-login.component';
+import { SellerRegistrationComponent } from './SellerComponents/seller-registration/seller-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserRegistrationComponent,
+    SellerLoginComponent,
+    SellerRegistrationComponent
   ],
   imports: [
     BrowserModule,
