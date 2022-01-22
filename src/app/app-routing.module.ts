8acb4ff9fc2fd72500e61a86c29a79fd2393b6c0
RUPAM LAHA <rupamlaha@RUPAMs-MacBook-Air.local>
2022-01-22 13:31:52 +0530

    user-registration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './UserComponents/user-login/user-login.component';
+import { UserRegistrationComponent } from './UserComponents/user-registration/user-registration.component';
 
 const routes: Routes = [
-  {path: 'user-login', component: UserLoginComponent}
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'user-registration', component: UserRegistrationComponent}
 ];
 
 @NgModule({
