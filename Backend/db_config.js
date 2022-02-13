ad61b18e559914ef6677d26975b3fa20c0134d08
RupamLaha <rupamlaha9@gmail.com>
2022-02-13 21:50:31 +0530

    rmv pass for security rsn

--- Backend/db_config.js
+++ Backend/db_config.js
@@ -4,7 +4,8 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '143@india',
+    //for security reasons removing pass...
+    password: '',
     database: 'wowshop'
 })
 
